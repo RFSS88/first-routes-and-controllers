3b8d25e9a90f198f62ecac973068b256263f9cba
rafael silva <silvaserranor@gmail.com>
2021-02-08 18:48:14 -0500

    Inproved update controller, added comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
             render json: user
         else
             render json: user.errors.full_messages, status: :unprocessable_entity
+            # 422 is the status code for an unprocessable entity.
+            # You can either pass the status code or status symbol.
+            # In other words, you can also return:
+            # render json: user.errors.full_messages, status: 422
         end
     end
 
@@ -20,9 +24,13 @@ class UsersController < ApplicationController
     end
 
     def update
-       @user = User.find(params[:id])
-       @user.update!(user_params)
-       render json: @user
+        user = User.find(params[:id])
+        
+        if user.update_attributes(user_params)
+            render json: user
+        else
+            render json: user.errors, status: 422
+        end
     end
 
     def destroy
